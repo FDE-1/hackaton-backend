8328565eb076d8bd7f1278a8ab694f2e6f68631d
Philemon3111 <99840844+Philemon3111@users.noreply.github.com>
2025-07-10 15:31:17 +0200

    feat: bon prompt

--- src/services/iaService.py
+++ src/services/iaService.py
@@ -1,9 +1,10 @@
 import requests 
+import json
+import re
 
-prompt_request = "– Donne moi un json sous cette forme : {\nMoyenne des notes parmi tous les avis récoltés (ex : note : 3,4),Nombre d’avis total (ex : 147),Les sujets les plus récurrents dans les avis sous format « top 5 » (ex : { 1 : « transport », 2 : « livraison », …}),Le Nombre d’avis négatif avec les avis associé (ex : { nb : 6, avis : { « c’est de la merde », « nul »…}}),Le Nombre d’avis neutre avec les avis associé (ex : { nb : 6, avis : { « c’est de la merde », « nul »…}}),Le Nombre d’avis positif avec les avis associé (ex : { nb : 6, avis : { « c’est de la merde », « nul »…}})}"
-
+prompt_request = "– Donne moi un json sous cette forme : {\nMoyenne des notes parmi tous les avis récoltés ,Nombre d’avis total ,Les sujets les plus récurrents dans les avis sous format « top 5 » ({ '1' : « transport », '2' : « livraison », …}),Le Nombre d’avis négatif avec les avis associé (ex : { 'nb' : 6, 'avis' : }),Le Nombre d’avis neutre avec les avis associé (ex : { 'nb' : 6, 'avis' : { }}),Le Nombre d’avis positif avec les avis associé (ex : { 'nb' : 6, 'avis' : { }})}. Le résultat devra être un json valide. Retourne moi uniquement je json. Lis bien tout ce que je t'envoie n'hallucine pas"
 def query_ollama(prompt: str):
-    final_prompt = prompt + prompt_request
+    final_prompt = json.dumps(prompt) + prompt_request
     response = requests.post('http://localhost:8000/generate', json={
         'model': 'deepseek-r1',
         'prompt': final_prompt,
@@ -11,4 +12,17 @@ def query_ollama(prompt: str):
     })
     if response.status_code != 200:
         raise Exception(response.text)
-    return response.json()["response"]
+    
+    response_data = response.json()
+    response_text = response_data.get("response", "")
+    
+    # Extraire le contenu entre ```json
+    json_match = re.search(r'```json\n([\s\S]*?)\n```', response_text)
+    if not json_match:
+        raise ValueError("Aucun contenu JSON trouvé dans la réponse")
+    
+    # Parser le contenu JSON extrait
+    json_content = json_match.group(1)
+    print(json_content)
+    return json.loads(json_content)
+    #return response.json()["response"]
